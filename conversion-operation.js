22f9dd1ff9a228c9f747b382451cebe97b62ce95
VanshKirtishahi <140483749+VanshKirtishahi@users.noreply.github.com>
2023-09-23 17:58:34 +0530

    Add files via upload

---
+++ conversion-operation.js
@@ -0,0 +1,52 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = "vansh"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false;
+//"" => false; 
+//"vansh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **********************OPERATIONS*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2); addition
+// console.log(2-2); substract
+// console.log(2*2); multiply
+// console.log(2**3); power
+// console.log(2/2); divide
+// console.log(2%2); remainder
+
+
+let str1 = "Hello"
+let str2 = " Vansh"
+
+let str3 = str1 + str2
+console.log( str3);
+
+
