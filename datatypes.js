fccd0dc5d6c3e4ce8efc649c08ae1d2967d36cf1
VanshKirtishahi <140483749+VanshKirtishahi@users.noreply.github.com>
2023-09-23 16:36:51 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all JS code as newer version of JS
+
+
+// alert (3 + 3) // We are using node.js no browser
+
+let name = "vansh"
+let age = 18
+let isLoggedIn = false
+let state; //undifened statement
+
+// number => 2 to power 53
+// bigint use for large no.
+// string => "" always Prefered
+// boolean => True or false statement
+//null => Standalone value
+//undifened => 
+//symbol => unique
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId = 144582
+let accountEmail ="vansh@gmail.com"
+var accountPasswaord = "123456"
+let accountState;
+
+// Prefer not to use var
+// Because issue in block scope({}) and functional scope
+
+accountCity = "Aurangabad"
+
+// accountId = 2 not allowed
+
+
+accountEmail = "jdfhfjd@fjsdjfgshdgj.com"
+accountPasswaord = "48754567878787"
+accountCity ="kolhapur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPasswaord, accountCity, accountState])
